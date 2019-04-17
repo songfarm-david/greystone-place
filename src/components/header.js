e30a3761f5ade8ca5e01cbe230e9d366258d5374
David Gaskin <davidburkegaskin@gmail.com>
2019-04-16 17:09:02 -0700

    set up state logic between header and nav for menu drawer

--- src/components/header.js
+++ src/components/header.js
@@ -5,20 +5,63 @@ import headerStyles from './header.module.scss'
 import Nav from './nav'
 
 // TODO: Add social share icons to the header
+// let menuState = true;
+// function onClick() {
+//    menuState = !menuState
+//    console.log(menuState)
+//    return menuState
+// }
 
-const Header = ({ siteTitle, description }) => (
-  <div id={headerStyles.header}>
-    <p className={headerStyles.contactPhone}><a tel="905-574-2626" title="Call Greystone Place">905-574-2626</a></p>
-    <div id={headerStyles.headerContainer}>
-      <h1>
-        <Link to="/">
-          {siteTitle}
-        </Link>
-      </h1>
-      <p name="description">{description}</p>
-    </div>
-    <Nav />
-  </div>
-)
+export default class Header extends React.Component {
 
-export default Header
+   constructor(props) {
+      super(props)
+      this.state = {menuOpen: false}
+      this.toggleMenu = this.toggleMenu.bind(this)
+   }
+
+   toggleMenu() {
+		this.setState(state => ({
+			menuOpen: !state.menuOpen
+		}))
+		console.log(this.state.menuOpen);
+	}
+
+   render() {
+      const { siteTitle, description } = this.props
+      return (
+         <div id={headerStyles.header}>
+           <p className={headerStyles.contactPhone}><a tel="905-574-2626" title="Call Greystone Place">905-574-2626</a></p>
+           <button id="mobMenu" onClick={this.toggleMenu}>X</button>
+           <div id={headerStyles.marquee}>
+             <h1>
+               <Link to="/">
+                 {siteTitle}
+               </Link>
+             </h1>
+             <p name="description">{description}</p>
+           </div>
+           <Nav menuOpen={this.state.menuOpen} />
+         </div>
+      )
+   }
+
+}
+
+// const Header = ({ siteTitle, description }) => (
+//   <div id={headerStyles.header}>
+//     <p className={headerStyles.contactPhone}><a tel="905-574-2626" title="Call Greystone Place">905-574-2626</a></p>
+//     <button id="mobMenu" onClick={onClick}>X</button>
+//     <div id={headerStyles.marquee}>
+//       <h1>
+//         <Link to="/">
+//           {siteTitle}
+//         </Link>
+//       </h1>
+//       <p name="description">{description}</p>
+//     </div>
+//     <Nav menuOpen={menuState} />
+//   </div>
+// )
+
+// export default Header
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,29 +1,27 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { withPrefix } from 'gatsby'
+// import { withPrefix } from 'gatsby'
 
-import toggleNav from '../navigation.js'
-
-import navStyles from './nav.module.scss'
+// import navStyles from './nav.module.scss'
+import '../styles/nav.scss'
 
 export default class Nav extends React.Component {
 
-	componentDidMount() {
-		let navToggle = document.querySelector("[name='mobileNavTrigger']")
-		navToggle.addEventListener('click', function(){
-			alert('Cheers')
-		})
+	constructor(props) {
+		super(props)
+		console.log(this.props);
+		// this.toggleNav = this.toggleNav.bind(this)
 	}
 
 	render() {
 		return (
-			<nav id={navStyles.nav}>
-				<ul>
+			<nav id="nav">
+				<ul className={this.props.menuOpen ? "drawer-open" : ""}>
+					<li id="closeDrawer">X</li>
 					<li><Link to="/apartments" title="Apartments">Apts.</Link></li>
 					<li><Link to="/apartment-upgrade-packages" title="Apartment Upgrade Packages">Upgrades</Link></li>
 					<li><Link to="/contact" contact="Contact">Inquiries</Link></li>
 				</ul>
-				<a href="javascript:void(0)" id={navStyles.mobMenu} name="mobileNavTrigger">X</a>
 			</nav>
 		)
 	}
