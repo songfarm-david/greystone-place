2d75d52d0d6e40421e55d69cd4c264c97721e825
David Gaskin <davidburkegaskin@gmail.com>
2019-06-05 16:55:49 -0700

    hack video to reload itself after play

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ export default class IndexPage extends React.Component {
             <section id="video-section" className="section">
                <h1 className="screen-reader">Welcome to Greystone Place</h1>
                <p><span className="emphasized-text">Greystone Place</span> by <a href="https://www.cfshops.com/lime-ridge.html" title="Go to Limeridge Mall website" target="_blank">Limeridge mall</a> is a prestigious, upscale address offering safety, security and quiet enjoyment. Meticulously maintained and ideal for mature adults.</p>
-               <video id="greystoneVideo" height="auto" width="100%" poster={transparentImg} controls>
+               <video id="greystoneVideo" height="auto" width="100%" preload="auto" poster={transparentImg} controls>
                   <p>Your browser doesn't support web video. Here is a <a href={''}>link to the video</a> instead.</p>
                </video>
                {/* TODO: test fallback link*/}
