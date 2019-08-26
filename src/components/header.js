82157870f4a9161de9bd0931007ee6389d0b6648
David Gaskin <davidburkegaskin@gmail.com>
2019-08-26 11:12:22 -0700

    pre google analytics event tracking

--- src/components/header.js
+++ src/components/header.js
@@ -15,6 +15,7 @@ export default class Header extends React.Component {
          menuOpen: false
       }
       this.toggleMenu = this.toggleMenu.bind(this)
+
    }
 
    toggleMenu() {
@@ -32,6 +33,22 @@ export default class Header extends React.Component {
       }
    }
 
+   // TODO: Implement custom GA events for certain events, see Google doc for detail
+   trackCall = () => {
+      console.log('trackCall called');
+      // if (typeof ga == 'undefined') {
+      //    return;
+      // }
+      // ga('send', 'event', 'Call Us', 'call', 'Call Us')
+      // window.dataLayer = window.dataLayer || [];
+      // console.log('what is window.dataLayer', window.dataLayer);
+      // function gtag() {
+      //    window.dataLayer.push(arguments);
+      // }
+      // gtag('js', new Date());
+      // gtag('config', 'UA-86141289-3');
+   }
+
    render() {
       const { siteTitle, description } = this.props
       return (
@@ -46,7 +63,7 @@ export default class Header extends React.Component {
            </div>
            <Nav menuOpen={this.state.menuOpen} />
            <div id="contactBar" className="">
-             <p id="headerPhone"><a href="tel:+19055742626" title="Call Greystone Place"><span className="phone-icon"><FontAwesomeIcon icon={faPhone} /></span>&nbsp;905-574-2626</a></p>
+             <p id="headerPhone"><a onClick={this.trackCall} href="tel:+19055742626" title="Call Greystone Place"><span className="phone-icon"><FontAwesomeIcon icon={faPhone} /></span>&nbsp;905-574-2626</a></p>
            </div>
 
            <button id="mobMenu" onClick={this.toggleMenu} className="hamburger hamburger--spin" type="button">
